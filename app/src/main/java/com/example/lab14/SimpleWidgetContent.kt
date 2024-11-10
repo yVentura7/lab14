da1e4c748f268330aeb2cb1adf7bf19ceabbafd9
yVentura7 <168231053+yVentura7@users.noreply.github.com>
2024-11-10 18:55:42 -0500

    2

--- app/src/main/java/com/example/lab14/SimpleWidgetContent.kt
+++ app/src/main/java/com/example/lab14/SimpleWidgetContent.kt
@@ -1,6 +1,7 @@
 package com.example.lab14
 
 import android.content.Context
+import android.content.Intent
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.unit.dp
 import androidx.glance.Button
@@ -9,6 +10,7 @@ import androidx.glance.GlanceModifier
 import androidx.glance.GlanceTheme
 import androidx.glance.action.actionStartActivity
 import androidx.glance.appwidget.GlanceAppWidget
+import androidx.glance.appwidget.action.actionStartActivity
 import androidx.glance.appwidget.provideContent
 import androidx.glance.background
 import androidx.glance.layout.Alignment
@@ -21,16 +23,15 @@ import androidx.glance.text.Text
 class SimpleWidgetContent : GlanceAppWidget() {
 
     override suspend fun provideGlance(context: Context, id: GlanceId) {
-
         provideContent {
             GlanceTheme {
-                MyContent()
+                MyContent(context)
             }
         }
     }
 
     @Composable
-    private fun MyContent() {
+    private fun MyContent(context: Context) {
         Column(
             modifier = GlanceModifier.fillMaxSize()
                 .background(GlanceTheme.colors.background),
@@ -38,12 +39,40 @@ class SimpleWidgetContent : GlanceAppWidget() {
             horizontalAlignment = Alignment.CenterHorizontally
         ) {
             Text(text = "¿A donde quieres dirigirte?", modifier = GlanceModifier.padding(12.dp))
+
             Row(horizontalAlignment = Alignment.CenterHorizontally) {
                 Button(
-                    text = "Página Principal",
-                    onClick = actionStartActivity<MainActivity>()
+                    text = "Home",
+                    onClick = actionStartActivity(
+                        Intent(context, MainActivity::class.java).apply {
+                            putExtra("destination", "home")
+                        }
+                    )
                 )
-
+                Button(
+                    text = "Work",
+                    onClick = actionStartActivity(
+                        Intent(context, MainActivity::class.java).apply {
+                            putExtra("destination", "work")
+                        }
+                    )
+                )
+                Button(
+                    text = "Study",
+                    onClick = actionStartActivity(
+                        Intent(context, MainActivity::class.java).apply {
+                            putExtra("destination", "study")
+                        }
+                    )
+                )
+                Button(
+                    text = "Problems",
+                    onClick = actionStartActivity(
+                        Intent(context, MainActivity::class.java).apply {
+                            putExtra("destination", "problems")
+                        }
+                    )
+                )
             }
         }
     }
